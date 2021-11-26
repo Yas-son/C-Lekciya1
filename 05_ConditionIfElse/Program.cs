72fb52fa0e98eb9c8b8cdbab120336e3f073fe03
Yas-son <5trk@bk.ru>
2021-11-26 10:40:56 +0300

    Rename folders 2 attempt

---
+++ 05_ConditionIfElse/Program.cs
@@ -0,0 +1,19 @@
+﻿//Начало
+//Ввести имя пользователя
+//ЕСЛИ имя пользователя совпадает с “Маша” то…
+//поприветствовать по особенному
+//ИНАЧЕ поприветствовать по обычному
+//Конец
+
+Console.Write("Ведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
